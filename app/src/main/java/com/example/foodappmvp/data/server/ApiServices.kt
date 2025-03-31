fc013900673cce18f9e217d0d814eaec77de3c16
ghores <ghazimoradi.amin@gmail.com>
2025-04-01 00:54:02 +0330

    Implementation main ui

---
+++ app/src/main/java/com/example/foodappmvp/data/server/ApiServices.kt
@@ -0,0 +1,5 @@
+package com.example.foodappmvp.data.server
+
+interface ApiServices {
+
+}
--- app/src/main/java/com/example/foodappmvp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/foodappmvp/ui/home/HomeFragment.kt
@@ -1,12 +1,20 @@
 package com.example.foodappmvp.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.foodappmvp.R
 import com.example.foodappmvp.databinding.FragmentHomeBinding
+import com.jakewharton.rxbinding4.widget.textChanges
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -18,10 +26,38 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("CheckResult")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding.apply {
+            //Search
+            searchEdt.textChanges()
+                .skipInitialValue()
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if (it.toString().length > 1) {
+                        //Call Api
+                    }
+                }
+            //Filter
+            filterFood()
+        }
+    }
+
+    private fun filterFood() {
+        val filters = listOf('A'..'Z').flatten()
+        val adapter = ArrayAdapter(requireContext(), R.layout.item_spinner, filters)
+        adapter.setDropDownViewResource(R.layout.item_spinner_list)
+        binding.filterSpinner.adapter = adapter
+        binding.filterSpinner.onItemSelectedListener = object :OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //CAll api
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
 
         }
     }
